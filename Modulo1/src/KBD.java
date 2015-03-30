f7f835dae7136220d0ba662e58afa4bb615439fa
caronaam <amcarona@gmail.com>
2015-03-30 21:37:28 +0100

    Adicionada classe Kit e fez-se refrator da classe KBD

--- Modulo1/src/KBD.java
+++ Modulo1/src/KBD.java
@@ -1,11 +1,8 @@
-import isel.leic.usbio.UsbPort;
-import isel.leic.utils.Time;
-
 
 public class KBD {
-	public static final int KEY_MASK = 0x0F;
-	public static final int K_VAL_MASK = 0x80;
-	public static final int K_ACK_MASK = 0xFE;
+	public static final int KEY_MASK = 0x0F; // Máscara para obter os 4 bits, menor peso, do Kit
+	public static final int K_VAL_MASK = 0x80; // Máscara para obter o bit maior peso do valor de entrada do Kit
+	public static final int K_ACK_MASK = 0x01; // Máscara para emitir o bit de menor peso para o Kit
 	public static final char NONE = 0; // Valor retornado quando não há tecla.
 	private static final boolean SCAN_SOFTWARE = true; // Mudar na implementação 2.
 	private static char[] keys;
@@ -18,49 +15,67 @@ public class KBD {
 	 */
 	public static char getKey(){
 		int in_val = 0;
-		in_val = ~UsbPort.in();
+		in_val = Kit.in();
 		if ((in_val&K_VAL_MASK) == K_VAL_MASK){
+			
 			//aplicar mascara e obter a tecla
-			System.out.println("teste " + (in_val&KEY_MASK));
-			//K_ACK
-			UsbPort.out(K_ACK_MASK);
+			System.out.println("Valor da tecla " + (in_val&KEY_MASK));
+			
+			//Emite K_ACK
+			Kit.out(K_ACK_MASK);
+			
 			while(true){
-				if(( ~UsbPort.in()&K_VAL_MASK) != K_VAL_MASK){
-					UsbPort.out(0xFF);
+				if(( Kit.in()&K_VAL_MASK) != K_VAL_MASK){
+					Kit.clear();
 					break;
 				}
-				Time.sleep(100);
+				Kit.sleep(100);
 			}
 			System.out.println("Caracter pressionado: " + KBD.keys[in_val&KEY_MASK]);
+			
 			return KBD.keys[in_val&KEY_MASK];
 		}
-		//System.out.println("No char! Bye");
+		
 		return NONE;
 		
 	}
-	// Retorna quando a tecla for premida ou NONE após decorrido ‘timeout’ milisegundos.
+	
+	/**
+	 * Retorna quando a tecla for premida ou NONE após decorrido ‘timeout’ milisegundos.
+	 * @param timeout Em milisegundos
+	 * @return Devolve o caracter pressionado
+	 */
 	public static char waitKey(long timeout){
-		long startTime = Time.getTimeInMillis();
-		long currentTime = Time.getTimeInMillis();
+		long startTime = Kit.getTimeInMillis();
+		long currentTime = Kit.getTimeInMillis();
 		char returnedChar;
 		while (currentTime - startTime <= timeout) {
 			returnedChar = getKey();
 			if(returnedChar != NONE){
-				return returnedChar; 
+				
+				return returnedChar;
+				
 			}
 			//esperar um decimo do timeout
-			Time.sleep(timeout/10);
-			currentTime = Time.getTimeInMillis();
+			Kit.sleep(timeout/10);
+			currentTime = Kit.getTimeInMillis();
 		}
+		
 		return NONE;
 		
-	}
-	// Estabelece os valores iniciais no porto de saída. Chamado no início do programa.
+	} 
+	
+	/**
+	 * Estabelece os valores iniciais no porto de saída. Chamado no início do programa.
+	 */
 	public static void init(){
-		UsbPort.out(127);
-		KBD.initChars();
+		Kit.clear();
+		initChars();
 	}
 	
+	/**
+	 * Método privado que inicializa o mapeamento dos caracteres com os valores inteiros recebidos pelo Kit
+	 */
 	private static void initChars(){
 		KBD.keys = new char[]{'0', '4', '8', 'C', '1', '5', '9', 'D','2', '6', 'A', 'E', '3', '7', 'B', 'F'};
 	}
--- Modulo1/src/Main.java
+++ Modulo1/src/Main.java
@@ -1,19 +1,9 @@
-import isel.leic.usbio.UsbPort;
-
 
 public class Main {
 
 	public static void main(String[] args) {
-		// CONTADOR
-		//Main.contador();
-		
 		//KBD
-		Main.kbd();
-	}
-	
-	private static void contador(){
-		UsbPort.out(0xff);
-		Contador.count();
+		kbd();
 	}
 	
 	private static void kbd(){
